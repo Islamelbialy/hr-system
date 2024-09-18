ba49e6a183ea645fa053ece5abf36d07ed6e6f04
islamelbialy <islam.mohamed@mans.edu.eg>
2024-09-18 20:00:11 +0300

    ghrf

--- hrsystem/settings.py
+++ hrsystem/settings.py
@@ -142,3 +142,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/'  # Redirect to home after login
+LOGOUT_REDIRECT_URL = '/'  # Redirect to home after logout
+
+
